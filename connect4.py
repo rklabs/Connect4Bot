e7e44f6f9c871e1bd2be77ba870caa2f148dc114
Raju Kadam <rajulkadam@gmail.com>
2017-03-02 22:20:42 -0800

    Added doc string for all methods in connect4.py

--- connect4.py
+++ connect4.py
@@ -5,7 +5,7 @@ Each block in the board can be player_a or player_b
 or empty_block which are represented using 'X', 'O'
 and '*' respectively. Winner is determined by checking
 4 directions(horizontal, vertical, diagonal(/),
-diagonal(\)) in the 2D array. This game can be
+diagonal(\)) in the 2D list. This game can be
 independently run on the terminal.
 """
 import random
@@ -26,25 +26,30 @@ class Connect4:
         self.board_height = 6
 
     def build_new_board(self):
+        """Build new game board with empty blocks"""
         self.connect4_board = []
 
         for _ in range(self.board_height):
             self.connect4_board.append([self.empty_block] * (self.board_width))
 
     def rows(self):
+        """Return each row of the 2D list as generator"""
         for row in self.connect4_board:
             yield row
 
     def print_board(self):
+        """Print the current board state"""
         print('Current board')
 
         for row in self.connect4_board:
             print('|'.join(row))
 
     def choose_first_player(self):
+        """Randomnly choose the first player for game simluation"""
         return random.choice([self.player_a, self.player_b])
 
     def make_move(self, player, column):
+        """Choose empty block in column"""
         row = self.board_height - 1
 
         while row > 0 and \
@@ -58,9 +63,11 @@ class Connect4:
         return True
 
     def choose_random_column(self):
+        """Choose random column for game simulation"""
         return random.randint(0, self.board_width - 1)
 
     def is_board_full(self):
+        """Check if board is full(tie)"""
         cnt = 0
 
         for x in range(self.board_height):
@@ -72,6 +79,7 @@ class Connect4:
         return cnt == (self.board_width * self.board_height)
 
     def check_row(self, player):
+        """Check all rows in board for four continuous blocks"""
         # Check rows for 4 continuous self.player_a or self.player_b
         for y in range(self.board_width):
             for x in range(self.board_height - 3):
@@ -82,6 +90,7 @@ class Connect4:
                     return True
 
     def check_column(self, player):
+        """Check all columns in board for four continuous blocks"""
         # Check columns for 4 continuous self.player_a or self.player_b
         for x in range(self.board_height):
             for y in range(self.board_width - 3):
@@ -92,7 +101,8 @@ class Connect4:
                     return True
 
     def check_diagonal_left_to_right(self, player):
-        # Check diagonal for 4 continous self.player_a or self.player_b
+        """Check all diagonals(\) in board for four continuous blocks"""
+        # Check diagonal for 4 continuous self.player_a or self.player_b
         # Diagonal from top right to bottom left
         for x in range(self.board_height - 3):
             for y in range(3, self.board_width - 1):
@@ -103,6 +113,7 @@ class Connect4:
                     return True
 
     def check_diagonal_right_to_left(self, player):
+        """Check all diagonals(/) in board for four continuous blocks"""
         # Diagonal from top left to bottom right
         for x in range(self.board_height - 3):
             for y in range(self.board_width - 3):
@@ -113,6 +124,7 @@ class Connect4:
                     return True
 
     def check_winner(self, player):
+        """Check all 4 directions for continuous similar blocks"""
         winner_check_funcs = [functools.partial(self.check_row, player),
                               functools.partial(self.check_column, player),
                               functools.partial(
@@ -128,6 +140,7 @@ class Connect4:
         return False
 
     def is_column_full(self, column):
+        """Check if column is full(and does not have empty block)"""
         column_elements = map(lambda row: row[column], self.connect4_board)
         empty_elements = filter(lambda elem: elem == self.empty_block,
                                 column_elements)
